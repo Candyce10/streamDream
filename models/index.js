9353ac0fadab37018eea8600e1140dc942818873
Kaye Bryant <bryantk72391@gmail.com>
2022-05-03 08:15:07 -0400

    trying a different mongoose.connect method as per mongoosejs.com

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
+// mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
+mongoose.connect('mongodb://localhost:4000/streamDream');
 
 module.exports = {
     StreamDream: require('./StreamDream'),
