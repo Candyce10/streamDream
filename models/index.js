2f6d901f1e2039fd737462bf795eb38a171ebead
Kaye Bryant <bryantk72391@gmail.com>
2022-05-03 08:17:38 -0400

    changed connect statement back

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
-// mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
-mongoose.connect('mongodb://localhost:4000/streamDream');
+mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
 
 module.exports = {
     StreamDream: require('./StreamDream'),
