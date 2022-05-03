62c130754b43f2f796d442898d551882dd7fdd6d
Kaye Bryant <bryantk72391@gmail.com>
2022-05-03 08:09:14 -0400

    movevd connection string above module.exports in index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
+mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
+
 module.exports = {
     StreamDream: require('./StreamDream'),
     Comment: require('./Comment'),
     Song: require('./Song'),
 }
 
-mongoose.connect(process.env.MONGODB_URI || "http://localhost:4000/streamDream")
