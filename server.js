79fb58f752c3722a20080759fc2a7f06446497e4
Kaye Bryant <bryantk72391@gmail.com>
2022-05-03 08:36:25 -0400

    requiring mongoose, heroku says it should be there

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const controllers = require('./controllers');
-
+const mongoose = require('mongoose');
 
 const app = express();
 
